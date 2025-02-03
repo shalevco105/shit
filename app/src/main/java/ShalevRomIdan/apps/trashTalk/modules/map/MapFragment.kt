bc34988b5878641c814c592d8fe65b11ca961ee2
Shalev <shalevco105@gmail.com>
2025-02-03 19:38:50 +0200

    add map

---
+++ app/src/main/java/ShalevRomIdan/apps/trashTalk/modules/map/MapFragment.kt
@@ -0,0 +1,42 @@
+package com.yourpackage.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.yourpackage.R
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private var mMap: GoogleMap? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Example: Add a marker at Auckland Sky Tower
+        val landmark = LatLng(-36.8485, 174.7633)
+        mMap?.addMarker(MarkerOptions().position(landmark).title("Auckland Sky Tower"))
+        mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(landmark, 15f))
+    }
+}
