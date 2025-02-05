1c9ba1d197e61a87fede56709cbb0a633fa4f6ca
Shalev <shalevco105@gmail.com>
2025-02-05 19:09:01 +0200

    fix map

--- app/src/main/java/ShalevRomIdan/apps/trashTalk/modules/map/MapFragment.kt
+++ app/src/main/java/ShalevRomIdan/apps/trashTalk/modules/map/MapFragment.kt
@@ -5,8 +5,10 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.location.Geocoder
 import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +17,7 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -25,6 +28,8 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import trashTalk.apps.trashTalk.R
+import trashTalk.apps.trashTalk.modules.TrashViewModel
+import java.util.Locale
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
@@ -96,6 +101,18 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
+    fun getLatLngFromAddress(address: String, callback: (LatLng?) -> Unit) {
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+        val addresses = geocoder.getFromLocationName(address, 1)
+        if (addresses != null && addresses.isNotEmpty()) {
+            val location = addresses[0]
+            val latLng = LatLng(location.latitude, location.longitude)
+            callback(latLng)
+        } else {
+            callback(null)
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.isBuildingsEnabled = true
@@ -104,29 +121,43 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         checkLocationPermission()
         moveToCurrentLocation()
 
-        //TODO: replace it with fetching trashes and display them
-
-        val israelPoints = listOf(
-            LatLng(31.7683, 35.2137),  // Jerusalem
-            LatLng(32.0853, 34.7818),  // Tel Aviv
-            LatLng(29.5577, 34.9519),  // Eilat
-            LatLng(32.7940, 34.9896),  // Haifa
-            LatLng(31.2529, 34.7915)   // Be'er Sheva
-        )
-
-        for (point in israelPoints) {
-            mMap?.addMarker(
-                MarkerOptions()
-                    .position(point)
-                    .title("Custom Marker")
-                    .icon(getBitmapDescriptor(R.drawable.ic_map))
-            )
+        val viewModel: TrashViewModel by lazy {
+            ViewModelProvider(this)[TrashViewModel::class.java]
         }
 
-        // Move the camera to the first point
-        if (israelPoints.isNotEmpty()) {
-            mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(israelPoints[0], 7f))
+        viewModel.trashes?.observe(viewLifecycleOwner) { trashesList ->
+            for (trash in trashesList) {
+                getLatLngFromAddress(trash.address) { latLng ->
+                    if (latLng != null) {
+                        val marker = mMap.addMarker(
+                            MarkerOptions()
+                                .position(latLng)
+                                .title(trash.name)
+                                .snippet(trash.address)
+                        )
+                        marker?.tag = trash
+                    }
+                }
+            }
         }
+
+
+//        val israelPoints = listOf(
+//            LatLng(31.7683, 35.2137),  // Jerusalem
+//            LatLng(32.0853, 34.7818),  // Tel Aviv
+//            LatLng(29.5577, 34.9519),  // Eilat
+//            LatLng(32.7940, 34.9896),  // Haifa
+//            LatLng(31.2529, 34.7915)   // Be'er Sheva
+//        )
+//
+//        for (point in israelPoints) {
+//            mMap?.addMarker(
+//                MarkerOptions()
+//                    .position(point)
+//                    .title("Custom Marker")
+//                    .icon(getBitmapDescriptor(R.drawable.ic_map))
+//            )
+//        }
 
         // Handle marker clicks
         mMap?.setOnMarkerClickListener { marker ->
